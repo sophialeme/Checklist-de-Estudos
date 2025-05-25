42390c3b3512567c4740c47f08d166bf7bf2dbe7
Sophia Leme <sophia.lemet1@gmail.com>
2025-05-25 15:09:04 -0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,50 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [nome, setNome] = useState("")
+  const [idade, setIdade] = useState("")
+  const [pessoas, setPessoas] = useState([])
+
+  const handleClick = () => {
+    if (nome && idade) {
+      setPessoas([...pessoas, { id: pessoas.length, nome, idade }])
+      setNome("")
+      setIdade("")
+    }
+  }
+
+  const handleNome = (e) => {
+    setNome(e.target.value)
+  }
+
+  const handleIdade = (e) => {
+    setIdade(e.target.value)
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='card'>
+      <input
+        type="text"
+        placeholder='Digite o nome'
+        onChange={handleNome}
+        value={nome}
+      />
+      <input
+        type="number"
+        placeholder='Digite a idade'
+        onChange={handleIdade}
+        value={idade}
+      />
+      <button onClick={handleClick}>Clique aqui</button>
+
+      {pessoas.map((pessoa) => (
+        <div key={pessoa.id}>
+          <p>Nome: {pessoa.nome}</p>
+          <p>Idade: {pessoa.idade}</p>
+        </div>
+      ))}
+    </div>
   )
 }
 
